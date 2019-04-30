6cd6c4cd5999ef84ec67f1171185437333c06dc3
Maciej Zalewski <38260620+Zalinafali@users.noreply.github.com>
2019-04-30 15:32:31 +0200

    Add files via upload

---
+++ Iterator/DataFrame.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFrames
+{
+    public class Record
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public Record()
+        { }
+        public Record(string name, string surname, string gender, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            Age = age;
+        }
+
+    }
+
+    public abstract class DataFrame : Iterator.IIterable
+    {
+        public abstract Iterator.IIterator GetIterator();
+    }
+
+    public class ListDataFrame : DataFrame, Iterator.IIterable
+    {
+        List<Record> list;
+
+        public ListDataFrame(List<Record> list)
+        {
+            this.list = new List<Record>(list);
+        }
+
+        public override Iterator.IIterator GetIterator()
+        {
+            return new ListDataFrameIterator(this);
+        }
+
+        private class ListDataFrameIterator : Iterator.IIterator
+        {
+            int index;
+            ListDataFrame list;
+            public ListDataFrameIterator(ListDataFrame ldf)
+            {
+                index = 0;
+                this.list = ldf;
+            }
+            public Record Current()
+            {
+                if (index < list.list.Count())
+                {
+                    return list.list.ElementAt(index++);
+                }
+                else
+                    return null;
+            }
+
+            public bool Next()
+            {
+                if (index < list.list.Count())
+                    return true;
+                else
+                    return false;
+            }
+        }
+    }
+
+    public class FileDataFrame : DataFrame, Iterator.IIterable
+    {
+        string filename;
+
+        public FileDataFrame(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public override Iterator.IIterator GetIterator()
+        {
+            return new FileDataFrameIterator(this);
+        }
+
+        private class FileDataFrameIterator : Iterator.IIterator
+        {
+            string data;
+            StreamReader file;
+
+            public FileDataFrameIterator(FileDataFrame fdf)
+            {
+                string CombinedPath = Path.Combine(Directory.GetCurrentDirectory(),  "../../" + fdf.filename);
+                this.file = new StreamReader(Path.GetFullPath(CombinedPath));
+            }
+
+            public Record Current()
+            {
+                if ((data = file.ReadLine()) == null)
+                {
+                    Console.WriteLine("error current");
+                    return null;
+                }
+                string[] features = new string[4];
+                features = data.Split(',');
+                return new Record(features[0], features[1], features[2], Convert.ToInt32(features[3]));
+            }
+
+            public bool Next()
+            {
+                return !file.EndOfStream;
+            }
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFrames
+{
+    class Program
+    {
+        static void PrintDataFrame(DataFrame df)
+        {
+            Iterator.IIterator it = df.GetIterator();
+            do
+            {
+                Record cr = it.Current();
+                if (cr == null) return;
+                Console.WriteLine(cr.Name + " " + cr.Surname + " " + cr.Gender + " " + cr.Age);
+            }
+            while (it.Next());
+        }
+
+        static Tuple<string, double> Mode(DataFrame df)
+        {
+            int male = 0;
+            int female = 0;
+            Iterator.IIterator it = df.GetIterator();
+            do
+            {
+                Record cr = it.Current();
+                if (cr == null) return null;
+                if (cr.Gender.Contains("M")) male++;
+                else female++;
+            }
+            while (it.Next());
+
+            string most = "";
+            double ratio = 0;
+            if (male > female)
+            {
+                most = "Male";
+                ratio = (double)male * 100 / (female + male);
+            }
+            else
+            {
+                most = "Female";
+                ratio = (double)female * 100 / (female + male);
+            }
+
+            return Tuple.Create(most, ratio);
+        }
+
+
+
+        //static //... ConcatenationWithCondition//...
+        //{
+        //    //...
+        //}
+
+        static void Main(string[] args)
+        {
+            List < Record > people = new List<Record>()
+            {
+                new Record("Keanu", "Reeves", "M", 25),
+                new Record("Agent", "Smith", "M", 35),
+                new Record("Carrie-Anne", "Moss", "F", 25),
+                new Record("Gloria", "Foster", "M", 123),
+                new Record("Belinda", "McClory", "F", 27)
+            };
+            ListDataFrame ldf = new ListDataFrame(people);
+            PrintDataFrame(ldf);
+
+            Tuple<string, double> res = Mode(ldf);
+            Console.WriteLine($"mode = {res.Item1}, percent = {res.Item2}");
+
+            FileDataFrame fdf = new FileDataFrame("dataFrame1.txt");
+            PrintDataFrame(fdf);
+            Tuple<string, double> res2 = Mode(fdf);
+            Console.WriteLine($"mode = {res2.Item1}, percent = {res2.Item2}");
+            //Console.WriteLine("\nConcatenation\n");
+            //Func<Record, bool> f = r => r.Gender == "F";
+            //PrintDataFrame(new ListDataFrame(ConcatenationWithCondition(ldf, fdf, f)));
+            //FileDataFrame fdf2 = new FileDataFrame(ConcatenationWithCondition(ldf, fdf, f), "dataFrame2.txt");
+        }
+    }
+}
